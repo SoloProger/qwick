f78ef56fe0d4cbfac5e6ab7e8e1a92d5f3d9c984
andreisolonnikov <asolonnikow@gmail.com>
2024-10-26 17:30:54 +0300

    Добавил switch компонент

--- src/shared/ui/switch/switch.component.ts
+++ src/shared/ui/switch/switch.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-switch',
@@ -6,17 +7,54 @@ import { Component, OnInit } from '@angular/core';
     <label class="switch">
       <input
         type="checkbox"
+        [value]="value"
+        (input)="update($event)"
+        [disabled]="disabled"
       />
       <span class="slider round"></span>
     </label>
-<!--    [value]="inputFormValue"-->
-    <!--    (input)="update($event)"-->
   `,
   standalone: true,
   styleUrl: './switch.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => SwitchComponent),
+    },
+  ],
 })
-export class SwitchComponent implements OnInit {
-  constructor() {}
+export class SwitchComponent implements ControlValueAccessor {
+  public value: boolean = false;
 
-  ngOnInit() {}
+  public onChange = (value: boolean) => {};
+
+  public onTouched = () => {};
+
+  public touched = false;
+
+  public disabled = false;
+
+  public writeValue(v: boolean): void {
+    this.value = v;
+  }
+
+  public registerOnChange(onChange: (v: boolean) => void): void {
+    this.onChange = onChange;
+  }
+
+  public registerOnTouched(onTouched: () => void): void {
+    this.onTouched = onTouched;
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  public update(event: Event): void {
+    const value = (event.target as HTMLInputElement).checked;
+    this.value = value;
+    this.onChange(value);
+    this.onTouched();
+  }
 }
